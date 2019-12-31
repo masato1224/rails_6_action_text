d70fd33baf2e72ea9261fe1e24874704304e5bc3
masa1224 <masatomasato1224@gmail.com>
2020-01-01 01:36:11 +0900

    add validation for content

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,22 @@ class Post < ApplicationRecord
 
   validates :title, length: {maximum:32}
   validates :title, presence: true
+
+  validate :validate_content_lenght
+
+  MAX_CONTENT_LENGHT = 50
+
+  def validate_content_lenght
+    length = content.to_plain_text.size
+
+    if length > MAX_CONTENT_LENGHT
+      errors.add(
+        :content, 
+        :too_long, 
+        max_content_lenght: MAX_CONTENT_LENGHT,
+        length: length
+      )
+    end
+  end
+
 end
