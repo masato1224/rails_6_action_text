8d35139225df832f59cbd87405a5ece6da9fa0a8
masa1224 <masatomasato1224@gmail.com>
2020-01-03 18:14:45 +0900

    add validation of files count

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,9 +6,11 @@ class Post < ApplicationRecord
 
   validate :validate_content_lenght
   validate :validate_content_attached_files_byte_size
+  validate :validate_content_attached_files_count
 
   MAX_CONTENT_LENGHT = 50
   MAX_TOTALL_BYTE_ATTACHED_FILES = 3.megabytes
+  MAX_ATTACHED_FILES_COUNT = 4
 
   def attached_files
     content.body.attachables.grep(ActiveStorage::Blob).uniq
@@ -43,6 +45,17 @@ class Post < ApplicationRecord
       :content_attached_files_byte_size_is_too_large,
       max_totall_byte_attached_files: MAX_TOTALL_BYTE_ATTACHED_FILES,
       totall_byte: totall_byte
+    )
+  end
+
+  def validate_content_attached_files_count
+    return if attached_files.length <= MAX_ATTACHED_FILES_COUNT
+
+    errors.add(
+      :base,
+      :content_attached_files_count_is_over_limit,
+      max_attached_files_count: MAX_ATTACHED_FILES_COUNT,
+      files_count: attached_files.length
     )
   end
 end
